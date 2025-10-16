b410f23a94856c9e6f3496771d848f03e6fd31ef
Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-10-16 08:03:37 -0400

    Remove credentials from GCS backend
    Removed credentials from backend configuration.

--- backend.tf
+++ backend.tf
@@ -2,6 +2,5 @@ terraform {
   backend "gcs" {
     bucket = "appenginebuckky1"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
-    credentials="testapp-472401-3d90d404d7bb.json"
   }
 }
