a9de9710c31308852065c130db842f0e838eef90
Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-10-16 08:01:04 -0400

    Update GCS credentials in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket = "appenginebuckky1"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
-    credentials="testapp-472401-563b7b1f59af.json"
+    credentials="testapp-472401-3d90d404d7bb.json"
   }
 }
