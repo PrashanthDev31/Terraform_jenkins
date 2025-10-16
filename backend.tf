7b8b4a737819eeee82a5a7e58e07bdc8c6b455d7
Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-10-16 07:54:01 -0400

    Add credentials to GCS backend configuration

--- backend.tf
+++ backend.tf
@@ -2,5 +2,7 @@ terraform {
   backend "gcs" {
     bucket = "appenginebuckky1"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
+    credentials="testapp-472401-563b7b1f59af.json
+"
   }
 }
