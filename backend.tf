5475e3eb29263df59d6f9567a4ec47a202a2986a
Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-10-16 07:56:16 -0400

    Fix formatting of credentials line in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "gcs" {
     bucket = "appenginebuckky1"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
-    credentials="testapp-472401-563b7b1f59af.json
-"
+    credentials="testapp-472401-563b7b1f59af.json"
   }
 }
