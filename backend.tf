eb879fbc32905634fbc9e971054be173a6122cee
Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-10-16 07:49:29 -0400

    Change GCS bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "storagestatefile"  # GCS bucket name
+    bucket = "appenginebuckky1"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
   }
 }
