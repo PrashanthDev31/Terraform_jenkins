78dc4429e2d02258a9ddff815ca749a16e55b876
Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-10-17 07:17:48 -0400

    Rename resources for VPC and subnet configurations

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 # 1. VPC Configuration
-resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-network"
+resource "google_compute_network" "vpc_network-practicecreation" {
+  name                    = "vpc-network-oct17-7am"
   auto_create_subnetworks = false
 }
 
 # 2. Subnet Configuration
-resource "google_compute_subnetwork" "subnet" {
-  name          = "subnet-1"
+resource "google_compute_subnetwork" "subnet-practicecreation" {
+  name          = "subnet-1-oct17-7am"
   region        = "us-central1"  # Updated region
   network       = google_compute_network.vpc_network.name
   ip_cidr_range = "10.0.0.0/24"
 }
 
-resource "google_storage_bucket" "importbucket07" {
-  name = "bucketjenkinsterracreated"
+resource "google_storage_bucket" "storage_bucket-practicecreation" {
+  name = "bucketjenkinsterracreated-oct17-7am"
   location = "US"
 }
