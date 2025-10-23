398ba242e8c4c7a79a96c3e7f1283bfc0ee550ac
Lakshmi Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-10-23 16:15:32 -0400

    Refactor VPC and subnet resource names in main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 # 1. VPC Configuration
-resource "google_compute_network" "vpc_network-practicecreation" {
-  name                    = "vpc-network-oct17-7am"
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-network"
   auto_create_subnetworks = false
 }
 
 # 2. Subnet Configuration
-resource "google_compute_subnetwork" "subnet-practicecreation" {
-  name          = "subnet-1-oct17-7am"
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet"
   region        = "us-central1"  # Updated region
-  network       = google_compute_network.vpc_network-practicecreation.name
+  network       = google_compute_network.vpc_network.name
   ip_cidr_range = "10.0.0.0/24"
 }
 
-resource "google_storage_bucket" "storage_bucket-practicecreation" {
-  name = "bucketjenkinsterracreated-oct17-7am"
+resource "google_storage_bucket" "storage_bucket" {
+  name = "bucketjenkinsterra"
   location = "US"
 }
